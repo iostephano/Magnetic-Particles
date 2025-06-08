3f8a3a3257d775e29479c6dc6fac9b14e0162ac9
Stephano Portella <iostephano@icloud.com>
2025-06-08 14:03:36 +0200

    Add files via upload

---
+++ AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  Magnetic Particles
+//
+//  Created by Stephano Portella on 04/06/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) { }
+}
---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magnetic Particles
+//
+//  Created by Stephano Portella on 04/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private var magneticView: MagneticParticlesView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        magneticView = MagneticParticlesView(frame: view.bounds)
+        magneticView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(magneticView)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        magneticView.frame = view.bounds
+    }
+}
